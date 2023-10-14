77f624e58bc25359e3a52aab8d0786feafc522b3
Mitchell <mitchell.stockley@gmail.com>
2023-10-14 11:58:00 +0200

    created the movies data access object

--- Chapter7/movies-reviews/backend/index.js
+++ Chapter7/movies-reviews/backend/index.js
@@ -1,6 +1,7 @@
 import app from './server.js';
 import mongodb from "mongodb";
 import dotenv from "dotenv";
+import MoviesDAO from './dao/moviesDAO.js';
 
 async function main() {
     dotenv.config(); //load in the environment variables
@@ -13,6 +14,7 @@ async function main() {
     try {
         //await connection to the MOngo cluster
         await client.connect(); //returns a promise //await is used to indicate that we block further execution untill completion 
+        await MoviesDAO.injectDB(client); //right after we connect to the database and just before we connect to the server we call inject to get our annitial reference to the movies collection in the database.
         //start server if no errors 
         app.listen(port, () => {
             console.log('Server is running and listening on port: ' + port);
