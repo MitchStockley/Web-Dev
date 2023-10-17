b0190ea2a44af0efc39f38794ee808040da7864f
Mitchell <mitchell.stockley@gmail.com>
2023-10-17 12:23:45 +0200

    updated code

--- Chapter14/frontend/src/App.js
+++ Chapter14/frontend/src/App.js
@@ -2,11 +2,44 @@ import React from 'react';
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+//importing our components
+import AddReview from "./components/add-review";
+import MoviesList from "./components/movies-list";
+import Movie from "./components/movie";
+import Login from "./components/login";
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 function App() {
+  const [user, setUser] = React.useState(null) //lets us add local state to our components// declares a state variable
+  async function login(user = null) {// default user to null
+    setUser(user)
+  }
+  async function logout() {
+    setUser(null)
+  }
   return (
     <div className="App">
-      Hello World
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {user ? (
+                <a onClick={logout}>Logout User</a>
+              ) : (
+                <Link to={"/login"}>Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
