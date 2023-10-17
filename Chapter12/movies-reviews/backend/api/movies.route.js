dfdda88d8e4c90e652c417954e70c52716cd93f8
Mitchell <mitchell.stockley@gmail.com>
2023-10-17 08:53:19 +0200

    completed backend

--- Chapter12/movies-reviews/backend/api/movies.route.js
+++ Chapter12/movies-reviews/backend/api/movies.route.js
@@ -5,6 +5,8 @@ import ReviewsController from "./reviews.controller.js";
 const router = express.Router(); // get access to express router
 
 router.route("/").get(MoviesController.apiGetMovies);
+router.route("/id/:id").get(MoviesController.apiGetMovieById) //retrieves specific movie and the reviews for that movie
+router.route("/ratings").get(MoviesController.apiGetRatings) //returns list of movie ratings //pg R etc
 
 router
   .route("/review")
--- Chapter12/movies-reviews/backend/dao/moviesDAO.js
+++ Chapter12/movies-reviews/backend/dao/moviesDAO.js
@@ -1,3 +1,6 @@
+import mongodb from "mongodb";
+
+const ObjectId = mongodb.ObjectId
 let movies;
 
 export default class MoviesDAO {
@@ -15,28 +18,63 @@ export default class MoviesDAO {
         filters = null,
         page = 0,
         moviesPerPage = 20, // will only get 20 movies at once
-    } = {}){
-    let query
-    if (filters) {
-        if ("title" in filters) {
-            query = { $text: { $search: filters['title'] } }
-        } else if ("rated" in filters) {
-            query = { "rated": { $eq: filters['rated'] } }
+    } = {}) {
+        let query
+        if (filters) {
+            if ("title" in filters) {
+                query = { $text: { $search: filters['title'] } }
+            } else if ("rated" in filters) {
+                query = { "rated": { $eq: filters['rated'] } }
+            }
+        }
+        let cursor
+        try {
+            cursor = await movies
+                .find(query)
+                .limit(moviesPerPage)
+                .skip(moviesPerPage * page)
+            const moviesList = await cursor.toArray()
+            const totalNumMovies = await movies.countDocuments(query)
+            return { moviesList, totalNumMovies }
+        }
+        catch (e) {
+            console.error(`Unable to issue find command, ${e}`)
+            return { moviesList: [], totalNumMovies: 0 }
         }
     }
-    let cursor
-    try {
-        cursor = await movies
-            .find(query)
-            .limit(moviesPerPage)
-            .skip(moviesPerPage * page)
-        const moviesList = await cursor.toArray()
-        const totalNumMovies = await movies.countDocuments(query)
-        return { moviesList, totalNumMovies }
+    static async getRatings() {
+        let ratings = []
+        try {
+            ratings = await movies.distinct("rated")
+            return ratings
+        }
+        catch (e) {
+            console.error(`unable to get ratings, $(e)`)
+            return ratings
+        }
     }
-    catch (e) {
-        console.error(`Unable to issue find command, ${e}`)
-        return { moviesList: [], totalNumMovies: 0 }
+    static async getMovieById(id) {
+        try {
+            return await movies.aggregate([
+                {
+                    $match: {
+                        _id: new ObjectId(id),
+                    }
+                },
+                {
+                    $lookup:
+                    {
+                        from: 'reviews',
+                        localField: '_id',
+                        foreignField: 'movie_id',
+                        as: 'reviews',
+                    }
+                }
+            ]).next()
+        }
+        catch (e) {
+            console.error(`something went wrong in getMovieById: ${e}`)
+            throw e
+        }
     }
-}
 }
