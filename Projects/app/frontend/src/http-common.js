c3a0e2408a8394001db364cdda0f8114cf7f9387
Mitchell <mitchell.stockley@gmail.com>
2023-10-24 15:42:35 +0200

    update

--- Projects/app/frontend/src/http-common.js
+++ Projects/app/frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api", //change to render link for deploy
+  baseURL: "https://tutorialsbackend.onrender.com/", //change to render link for deploy
   headers: {
     "Content-type": "application/json"
   }
