3033e47b1b30b8c27affe289892cb3ff12f46dc7
Mitchell <mitchell.stockley@gmail.com>
2023-10-24 15:47:26 +0200

    update

--- Projects/app/frontend/src/services/tutorial.service.js
+++ Projects/app/frontend/src/services/tutorial.service.js
@@ -2,31 +2,31 @@ import http from "../http-common";
 
 class TutorialDataService {
   getAll() {
-    return http.get("/https://tutorialsbackend.onrender.com/api/tutorials/");
+    return http.get("https://tutorialsbackend.onrender.com/api/tutorials/");
   }
 
   get(id) {
-    return http.get(`/https://tutorialsbackend.onrender.com/api/tutorials//${id}`);
+    return http.get(`https://tutorialsbackend.onrender.com/api/tutorials/${id}`);
   }
 
   create(data) {
-    return http.post("/https://tutorialsbackend.onrender.com/api/tutorials/", data);
+    return http.post("https://tutorialsbackend.onrender.com/api/tutorials", data);
   }
 
   update(id, data) {
-    return http.put(`/https://tutorialsbackend.onrender.com/api/tutorials/${id}`, data);
+    return http.put(`https://tutorialsbackend.onrender.com/api/tutorials/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`/https://tutorialsbackend.onrender.com/api/tutorials/${id}`);
+    return http.delete(`https://tutorialsbackend.onrender.com/api/tutorials/${id}`);
   }
 
   deleteAll() {
-    return http.delete(`/https://tutorialsbackend.onrender.com/api/tutorials/`);
+    return http.delete(`https://tutorialsbackend.onrender.com/api/tutorials`);
   }
 
   findByTitle(title) {
-    return http.get(`/https://tutorialsbackend.onrender.com/api/tutorials/?title=${title}`);
+    return http.get(`https://tutorialsbackend.onrender.com/api/tutorials/?title=${title}`);
   }
 }
 
