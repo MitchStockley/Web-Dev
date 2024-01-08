b16cc0b1c1eb6aafebdcc4896ef7e3957a3a76bb
Mitchell <mitchell.stockley@gmail.com>
2024-01-08 16:16:43 +0200

    updated

--- Projects/tutorialAppLocal/frontend/src/services/tutorial.service.js
+++ Projects/tutorialAppLocal/frontend/src/services/tutorial.service.js
@@ -2,31 +2,31 @@ import http from "../http-common";
 
 class TutorialDataService {
   getAll() {
-    return http.get("/tutorials/");
+    return http.get("https://tutorialapp-4lgp.onrender.com/api/tutorials");
   }
 
   get(id) {
-    return http.get(`/tutorials/${id}`);
+    return http.get(`https://tutorialapp-4lgp.onrender.com/api/tutorials/${id}`);
   }
 
   create(data) {
-    return http.post("/tutorials", data);
+    return http.post("https://tutorialapp-4lgp.onrender.com/api/tutorials", data);
   }
 
   update(id, data) {
-    return http.put(`/tutorials/${id}`, data);
+    return http.put(`https://tutorialapp-4lgp.onrender.com/api/tutorials/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`/tutorials/${id}`);
+    return http.delete(`https://tutorialapp-4lgp.onrender.com/api/tutorials/${id}`);
   }
 
   deleteAll() {
-    return http.delete(`/tutorials`);
+    return http.delete(`https://tutorialapp-4lgp.onrender.com/api/tutorials`);
   }
 
   findByTitle(title) {
-    return http.get(`/tutorials/?title=${title}`);
+    return http.get(`https://tutorialapp-4lgp.onrender.com/api/tutorials/?title=${title}`);
   }
 }
 
